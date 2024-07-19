6b9f8684ec4d0ad21205c23728988409c7715214
Jonyski <geleiaeavela@gmail.com>
2024-07-19 04:49:56 -0300

    fixes code styling

--- src/modules/places.c
+++ src/modules/places.c
@@ -6,30 +6,24 @@
 #include <ctype.h>
 
 int printMap(){
-
 	FILE *file;
 	char mapa[1600];
-
 	file = fopen("/home/user/peregrino-visual-novel/assets/EACH-map.txt", "r");
-
 	if(file == NULL){
 		perror("Error opening file");
 		return EXIT_FAILURE;
 	}
 
 	while(fgets(mapa, sizeof(mapa), file) != NULL){
-
 		printf("%s", mapa);
 	}
-
+	
 	fclose(file);
 	return 1;
 }
 
 void showNavigationOptions(){
-
 	char choose[1];
-
 	slowPrint("Para onde você quer ir, então?\n");
 		printf("A)BIBLIOTECA     E)GRAMA\n");
 		printf("B)QUEIJO         F)CICLO BÁSICO\n");
@@ -40,7 +34,6 @@ void showNavigationOptions(){
 		system("clear");
 
 	switch(toupper(*choose)){
-
 		case 'A':
 			biblioteca();
 			break;
@@ -69,15 +62,14 @@ void showNavigationOptions(){
 }
 
 //FUNCTIONS WITH THE SWITCHES OF THE EACH ONE OF THE PLACES (IMPORTANT TO THE RECURSION & LOOPING)
-void switchbiblio(char *choose){
 
+void switchbiblio(char *choose){
 	printf("A)BANHEIRO DA BIBLIOTECA     B)ENTRAR NA BIBLIOTECA\n");
 	printf("C)IR PARA OUTRO LUGAR\n");
 	printf("Sua escolha: ");
 	scanf("%c", &choose[0]);
 
 	switch (toupper(*choose)){
-
 		case 'A':
 			slowPrint("Você caminha até o banheiro da Biblioteca!\n");
 			slowPrint("Depois de fazer suas necessidades, você volta para a entrada da Biblioteca.\n");
@@ -101,12 +93,9 @@ void switchbiblio(char *choose){
 			switchbiblio(choose);
 			break;			
 	}
-
 }
 
-
 void switchtitanic(char *choose){
-
 	printf("A)BANDECO           B)SALAS DE AULA\n");
 	printf("C)MESAS DE PAPELÃO  D)ASSENTOS DA ENTRADA\n");
 	printf("E)IR PARA OUTRO LUGAR\n");
@@ -114,7 +103,6 @@ void switchtitanic(char *choose){
 	scanf("%c", &choose[0]);
 
 	switch (toupper(*choose)){
-
 		case 'A':
 			slowPrint("Você anda em direção ao Bandeco...\n");
 			slowPrint("Mas, ele está fechado (trancado pelo outro lado).\n");
@@ -157,13 +145,11 @@ void switchtitanic(char *choose){
 }
 
 void switchqueijo(char *choose){
-
 	printf("A)SENTAR E REFLETIR SOBRE A VIDA    B)IR PARA OUTRO LUGAR\n");
 	printf("Sua escolha: ");
 	scanf("%c", &choose[0]);
 
 	switch(toupper(*choose)){
-
 		case 'A':
 			slowPrint("Você senta no Queijo e começa a transcender espiritualmente...\n");
 			slowPrint("Após isso, o que você vai fazer?\n");
@@ -180,11 +166,9 @@ void switchqueijo(char *choose){
 			switchqueijo(choose);
 			break;
 	}
-
 }
 
 void switchauditorios(char *choose){
-
 	printf("A)LANCHONETE     B)AUDITÓRIO 1\n");
 	printf("C)AUDITÓRIO 2	 D)BANHEIROS \n");
 	printf("E)IR PARA OUTRO LUGAR\n");
@@ -192,7 +176,6 @@ void switchauditorios(char *choose){
 	scanf("%c", &choose[0]);
 
 	switch (toupper(*choose)){
-
 		case 'A':
 			slowPrint("Você chega na lanchonete e começa a olhar os preços...\n");
 			slowPrint("Mas, percebe que os preços estão longe da realidade do brasileiro médio.\n");
@@ -200,7 +183,6 @@ void switchauditorios(char *choose){
 			slowPrint("Para onde, agora?\n");
 			switchauditorios(choose);
 			break;
-
 		case 'B':
 			slowPrint("Você anda até o primeiro auditório...\n");
 			slowPrint("Mas não tem nada rolando lá.\n");
@@ -208,7 +190,6 @@ void switchauditorios(char *choose){
 			slowPrint("Para onde, agora?\n");
 			switchauditorios(choose);
 			break;
-
 		case 'C':
 			slowPrint("Você anda até o segundo auditório...\n");
 			slowPrint("Mas não tem nada rolando lá.\n");
@@ -216,7 +197,6 @@ void switchauditorios(char *choose){
 			slowPrint("Para onde, agora?\n");
 			switchauditorios(choose);
 			break;
-		
 		case 'D':
 			slowPrint("Você anda até os banheiros...\n");
 			slowPrint("E tira água do joelho.\n");
@@ -224,83 +204,62 @@ void switchauditorios(char *choose){
 			slowPrint("Para onde, agora?\n");
 			switchauditorios(choose);
 			break;
-		
 		case 'E':
 			system("clear");
 			printMap();
 			showNavigationOptions();
 			break;
-
 		default:
 			system("clear");
 			slowPrint("Opção inválida. Escolha novamente.\n");
 			switchauditorios(choose);
 			break;
-
 	}
-
 }
 
 void switchgrama(char *choose){
-
 	printf("A)PEGAR UMA CADEIRA E SENTAR   B)IR PARA OUTRO LUGAR\n");
 	printf("Sua escolha: ");
 	scanf("%c", &choose[0]);
 
-
 	switch(toupper(*choose)){
-
 		case 'A':
 			slowPrint("Você pega uma cadeira, senta na grama e começa a relaxar...\n");
 			slowPrint("Depois de um tempo, você levanta e guarda a cadeira.\n");
 			slowPrint("O que vais fazer agora?\n");
 			switchgrama(choose);
 			break;
-
 		case 'B':
 			system("clear");
 			printMap();
 			showNavigationOptions();
-
 		default:
 			system("clear");
 			slowPrint("Opção inválida. Escolha novamente.\n");
 			switchgrama(choose);
 			break;
-
 	}
-
-
 }
 
 //FUNCTIONS THAT REPRESENT THE PLACES
 
-
 void biblioteca(){
-
 	char choose[1];
-
 	slowPrint("Após caminhar um pouco, você chegou na entrada da Biblioteca!\n");
 	slowPrint("Agora, o que fazer?\n");
 	switchbiblio(choose);
-
 }	
 
 
 void queijo(){
-
 	char choose[1];
-
 	slowPrint("Após caminhar um pouco, você chegou no Queijo!\n");
 	slowPrint("Agora, o que fazer?\n");
 	switchqueijo(choose);
-	
 }
 
 void auditorios(){
-
 	char choose[1];
-
 	slowPrint("Após caminhar um pouco, você chegou na entrada de acesso aos auditórios!\n");
 	slowPrint("Agora, aonde você quer ir?\n");
 	switchauditorios(choose);
@@ -308,9 +267,7 @@ void auditorios(){
 }
 
 void titanic(){
-
 	char choose[1];
-
 	slowPrint("Após caminhar um pouco, você chegou no titanic!\n");
 	slowPrint("Para onde, agora?\n");
 	switchtitanic(choose);
@@ -318,9 +275,7 @@ void titanic(){
 }
 
 void grama(){
-
 	char choose[1];
-
 	slowPrint("Após caminhar um pouco, você chegou na grama!\n");
 	slowPrint("Agora, o que fazer?\n");
 	switchgrama(choose);
@@ -328,21 +283,18 @@ void grama(){
 }
 
 int cb(){
-
 	printf("Após caminhar um pouco, você chegou no Ciclo Básico(CB)!\n");
 	printf("Para onde, agora?\n");
 	return 1;
 }
 
 int elefantebranco(){
-
 	printf("Após caminhar um pouco, você chegou no Elefante Branco!\n");
 	printf("Para onde, agora?\n");
 	return 1;
 }
 
 int ginasio(){
-
 	printf("Após caminhar um pouco, você chegou no Ginásio!\n");
 	printf("Para onde, agora?\n");
 	return 1;
