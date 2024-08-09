1deefd79ce9d7ea3b18636b08e737a58eaedc867
Caio Bernardo <caio.2004vb@gmail.com>
2024-08-09 17:24:30 -0300

    style(normalized intentation)
    use tabs instead of spaces, happy now johnny? motherfucker

--- src/modules/minigame.c
+++ src/modules/minigame.c
@@ -9,59 +9,59 @@ void calc_game();
 void ip_game();
 
 void minigameManager() {
-  currContext.currActivity = MINIGAME;
-  switch (currContext.miniGameId) {
-  case 0:
-    calc_game();
-    break;
-  case 1:
-    ip_game();
-    break;
-  default:
-    printf("Unimplemented game id\n");
-    break;
-  }
-  currContext.currActivity = CLASS;
+	currContext.currActivity = MINIGAME;
+	switch (currContext.miniGameId) {
+	case 0:
+		calc_game();
+		break;
+	case 1:
+		ip_game();
+		break;
+	default:
+		printf("Unimplemented game id\n");
+		break;
+	}
+	currContext.currActivity = CLASS;
 }
 
 void ip_game() {
-  printf("Unimplemented\n");
-  readInput();
+	printf("Unimplemented\n");
+	readInput();
 }
 
 void calc_game() {
 
-  currContext.miniGameQuestionId = 0;
-  printf("---PRIMEIRA QUESTÃO---\n\n");
-  printf("Hoje a aula foi sobre o conceito de infinidade. Com os seus "
-         "conhecimentos de Pré-Cálculo, escolha a alternativa correta:\n");
-  printf("Qual desses conjuntos numéricos é maior que todos os outros?\n");
-  printf("1) Naturais\n"
-         "2) Reais\n"
-         "3) Racionais\n"
-         "4) Inteiros\n");
-  printf("Digite o número correspondente a alternativa escolhida:\n");
+	currContext.miniGameQuestionId = 0;
+	printf("---PRIMEIRA QUESTÃO---\n\n");
+	printf("Hoje a aula foi sobre o conceito de infinidade. Com os seus "
+		   "conhecimentos de Pré-Cálculo, escolha a alternativa correta:\n");
+	printf("Qual desses conjuntos numéricos é maior que todos os outros?\n");
+	printf("1) Naturais\n"
+		   "2) Reais\n"
+		   "3) Racionais\n"
+		   "4) Inteiros\n");
+	printf("Digite o número correspondente a alternativa escolhida:\n");
 
-  readInput();
+	readInput();
 
-  currContext.miniGameQuestionId++;
+	currContext.miniGameQuestionId++;
 
-  printf("Resposta correta!\n");
+	printf("Resposta correta!\n");
 
-  printf("---SEGUNDA QUESTÃO---\n\n");
-  printf(" ");
+	printf("---SEGUNDA QUESTÃO---\n\n");
+	printf(" ");
 
-  readInput();
+	readInput();
 
-  currContext.miniGameQuestionId++;
-  printf("Resposta correta!\n");
+	currContext.miniGameQuestionId++;
+	printf("Resposta correta!\n");
 
-  printf("---TERCEIRA QUESTÃO---\n\n");
-  printf(" ");
+	printf("---TERCEIRA QUESTÃO---\n\n");
+	printf(" ");
 
-  readInput();
+	readInput();
 
-  printf("Resposta correta!\n");
+	printf("Resposta correta!\n");
 }
 
 InputERR processCalcGameQ1(char *input);
@@ -69,34 +69,34 @@ InputERR processCalcGameQ2(char *input);
 InputERR processCalcGameQ3(char *input);
 
 InputERR processCalcGame(char *input, short int question_num) {
-  switch (question_num) {
-  case 0:
-    return processCalcGameQ1(input);
-    break;
-  case 1:
-    return processCalcGameQ2(input);
-    break;
-  default:
-    return NO_ERR;
-    break;
-  }
+	switch (question_num) {
+	case 0:
+		return processCalcGameQ1(input);
+		break;
+	case 1:
+		return processCalcGameQ2(input);
+		break;
+	default:
+		return NO_ERR;
+		break;
+	}
 }
 
 InputERR processCalcGameQ1(char *input) {
-  if (!strlen(input)) {
-    return NULL_INPUT;
-  } else if (strlen(input) > 1) {
-    return TOO_LONG;
-  }
+	if (!strlen(input)) {
+		return NULL_INPUT;
+	} else if (strlen(input) > 1) {
+		return TOO_LONG;
+	}
 
-  int input_as_num = atoi(input);
-  if (input_as_num == 0) {
-    return SHOULD_BE_INT;
-  } else if (input_as_num != 2) {
-    return WRONG_ANSWER;
-  } else {
-    return NO_ERR;
-  }
+	int input_as_num = atoi(input);
+	if (input_as_num == 0) {
+		return SHOULD_BE_INT;
+	} else if (input_as_num != 2) {
+		return WRONG_ANSWER;
+	} else {
+		return NO_ERR;
+	}
 }
 
 // TODO: Unimplemented
