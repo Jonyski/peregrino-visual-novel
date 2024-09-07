3e04464087db786cf42d17b5db471490392eecc3
Voltageseven <xextreme225@gmail.com>
2024-09-07 19:48:52 -0300

    dor e sofrimento

--- headers/navigation.h
+++ headers/navigation.h
@@ -1,27 +1,51 @@
-#ifndef PLACES_H
-#define PLACES_H
+#ifndef NAVIGATION_H
+#define NAVIGATION_H
+#include "navigation.h"
+#include <stddef.h>
 
+typedef struct place{
+	const char* name;
+	struct place* subPlaces[10];
+	char *filePath;
+	int numSubPlaces;
+	struct place* father;
+} place_t;
+
+		extern place_t banheirofb;
+		extern place_t banheiromb;
+		extern place_t estantes;
+	extern place_t biblioteca;
+		extern place_t ru;
+		extern place_t salaaula;
+		extern place_t spawn;
+		extern place_t papelao;
+		extern place_t banheiroft;
+		extern place_t banheiromt;
+		extern place_t telhado;
+	extern place_t titanic;
+		extern place_t anfiteatrocb;
+		extern place_t lab;
+		extern place_t cantinacb;
+		extern place_t banheiromcb;
+		extern place_t banheirofcb;
+		extern place_t rp;
+	extern place_t cb;
+		extern place_t quadra;
+		extern place_t aluguel;
+		extern place_t labirinto;
+		extern place_t academia;
+		extern place_t escalada;
+		extern place_t pingpong;
+	extern place_t ginasio;
+	extern place_t grama;
+	extern place_t queijo;
+	extern place_t auditorio;
+extern place_t each;
+
+
+void initialize_places();
+int navigatetest(place_t place);
 int navigateMap();            // loops the navigation cycle
 int printMap();               // exibits the map
 void showNavigationOptions(); // shows the each-general navigation options
-
-// functions that print what happens when you first enter each place
-// and calls its respective loop
-void biblioteca();
-void queijo();
-void auditorios();
-void titanic();
-void grama();
-int cb();
-int elefantebranco();
-int ginasio();
-
-// each place has a loop that asks what the player wants to do in that place
-void biblioLoop(char *);
-void titanicLoop(char *);
-void queijoLoop(char *);
-void auditoriosLoop(char *);
-void gramaLoop(char *);
-
-
 #endif
--- src/modules/navigation.c
+++ src/modules/navigation.c
@@ -3,19 +3,266 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stddef.h>
 #include "navigation.h"
-#include "narrator.h"
-#include "utils.h"
-#include "commands.h"
 
-bool navigationIsOver = false;
+		place_t banheirofb = {
+			.name = "Banheiro Feminino",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0
+			
+		};
 
-int navigateMap(){
-	navigationIsOver = false;
-	while(!navigationIsOver) {
-		printMap();
-		showNavigationOptions();
+		place_t banheiromb = {
+			.name = "Banheiro Masculino",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0
+			
+		};
+
+		place_t estantes = {
+			.name = "Estantes de livros",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0
+			
+		};
+
+	place_t biblioteca = {
+		.name = "Biblioteca",
+		.subPlaces = {&banheirofb, &banheiromb, &estantes},
+		.filePath = NULL,
+		.numSubPlaces = 3
+	};
+
+		place_t ru = {
+			.name = "Bandejão",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0
+		};
+		place_t salaaula = {
+			.name = "Sala de Aula",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0
+		};
+		place_t spawn = {
+			.name = "Spawnpoint",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0
+		};
+		place_t papelao = {
+			.name = "Papelão",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0
+		};
+		place_t banheiroft = {
+			.name = "Banheiro Feminino",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0
+		};
+		place_t banheiromt = {
+			.name = "Banheiro Masculino",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0
+		};
+		place_t telhado = {
+			.name = "Telhado",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0
+		};
+
+	place_t titanic = {
+		.name = "Titanic",
+		.subPlaces = {&ru, &salaaula, &spawn, &papelao, &banheiroft, &banheiromt, &telhado},
+		.filePath = NULL,
+		.numSubPlaces = 7	 
+	};
+
+		place_t anfiteatrocb = {
+			.name = "Anfiteatro",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0	 
+		};
+		place_t lab = {
+			.name = "Laboratório",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0	 
+		};
+		place_t cantinacb = {
+			.name = "Cantina",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0	 
+		};
+		place_t banheiromcb = {
+			.name = "Banheiro Masculino",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0	 
+		};
+		place_t banheirofcb = {
+			.name = "Banheiro Feminino",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0	 
+		};
+		place_t rp = {
+			.name = "Salas de RP",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0	 
+		};
+
+	place_t cb = {
+		.name = "CB",
+		.subPlaces = {&anfiteatrocb, &lab, &cantinacb, &banheirofcb, &banheiromcb, &rp},
+		.filePath = NULL,
+		.numSubPlaces = 6	 
+	};
+
+		place_t quadra = {
+			.name = "Quadra",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0	 
+		};
+		place_t aluguel = {
+			.name = "Empréstimo de equipamentos esportivos",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0	 
+		};
+		place_t labirinto = {
+			.name = "Labirinto",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0	 
+		};
+		place_t academia = {
+			.name = "Academia",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0	 
+		};
+		place_t escalada = {
+			.name = "Escalada",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0	 
+		};
+		place_t pingpong = {
+			.name = "Ping Pong",
+			.subPlaces = {NULL},
+			.filePath = NULL,
+			.numSubPlaces = 0	 
+		};
+
+	place_t ginasio = {
+		.name = "Ginásio",
+		.subPlaces = {&quadra, &aluguel, &labirinto, &academia, &escalada, &pingpong},
+		.filePath = NULL,
+		.numSubPlaces = 6 
+	};
+
+	place_t grama = {
+		.name = "Grama",
+		.subPlaces = {NULL},
+		.filePath = NULL,
+		.numSubPlaces = 0	 
+	};
+
+	place_t queijo = {
+		.name = "Queijo",
+		.subPlaces = {NULL},
+		.filePath = NULL,
+		.numSubPlaces = 0 
+	};
+
+	place_t auditorio = {
+		.name = "Auditório",
+		.subPlaces = {NULL},
+		.filePath = NULL,
+		.numSubPlaces = 0 
+	};
+
+place_t each = {
+	.name = "Each",
+	.subPlaces = {&biblioteca, &titanic, &cb, &ginasio, &grama, &queijo, &auditorio},
+	.filePath = NULL,
+	.numSubPlaces = 7
+};
+
+void initialize_places(){
+	biblioteca.father = &each;
+		banheirofb.father = &biblioteca;
+		banheiromb.father = &biblioteca;
+		estantes.father = &biblioteca;
+	titanic.father = &each;
+		ru.father = &titanic;
+		salaaula.father = &titanic;
+		spawn.father = &titanic;
+		papelao.father = &titanic;
+		banheiroft.father = &titanic;
+		banheiromt.father = &titanic;
+		telhado.father = &titanic;
+	cb.father = &each;
+		anfiteatrocb.father = &cb;
+		lab.father = &cb;
+		cantinacb.father = &cb;
+		banheiromcb.father = &cb;
+		banheirofcb.father = &cb;
+		rp.father = &cb;
+	ginasio.father = &each;
+		quadra.father = &ginasio;
+		aluguel.father = &ginasio;
+		labirinto.father = &ginasio;
+		academia.father = &ginasio;
+		escalada.father = &ginasio;
+		pingpong.father = &ginasio;
+	grama.father = &each;
+	queijo.father = &each;
+	auditorio.father = &each;
+}
+
+int navigatetest(place_t place){
+	initialize_places();
+
+	printf("Você está em %s. Para onde quer ir agora?\n", place.name);
+	if(place.numSubPlaces != 0){
+		for(int i=0; i<place.numSubPlaces; i++){
+			printf("%d) %s\n", i+1, place.subPlaces[i]->name);
+		}
+		if(place.father != NULL) printf("%d) Ir para outro lugar", place.numSubPlaces + 1);
+	} else {
+		printf("Morreu\n");
+		return 0;
 	}
+	printf(">");
+	int choice;
+	scanf("%d", &choice);
+
+	if(choice == place.numSubPlaces + 1 && place.father != NULL){
+
+		navigatetest(*place.father);
+
+	} else {
+
+		navigatetest(*place.subPlaces[choice - 1]);
+	}
+
+	return 1;
+
 }
 
 int printMap(){
@@ -36,367 +283,9 @@ int printMap(){
 	while(fgets(mapa, sizeof(mapa), file) != NULL){
 		printf("%s", mapa);
 	}
-	
+
 	fclose(file);
 	return 1;
 }
 
-void showNavigationOptions(){
-	char choice[USR_INPUT_MAX_SIZE];
-	slowPrint("Para onde você quer ir, então?\n");
-		printf("1)BIBLIOTECA     5)GRAMA\n");
-		printf("2)QUEIJO         6)CICLO BÁSICO\n");
-		printf("3)AUDITÓRIOS     7)ELEFANTE BRANCO\n");
-		printf("4)TITANIC        8)GINÁSIO\n");
-	
-	invalidInputLabel:
-	printf("> ");
-	cleanScan(choice);
-	while(classifiesAsCommand(choice)) {
-		clear();
-		processCommand(choice);
-		// I know, I know, code repetition is an unforgivable crime or something
-		slowPrint("Com isso fora do caminho, para onde você quer ir?\n");
-			printf("1)BIBLIOTECA     5)GRAMA\n");
-			printf("2)QUEIJO         6)CICLO BÁSICO\n");
-			printf("3)AUDITÓRIOS     7)ELEFANTE BRANCO\n");
-			printf("4)TITANIC        8)GINÁSIO\n");
-			printf("> ");
-		cleanScan(choice);
-	}
 
-	switch(toupper(*choice)){
-		case '1':
-			clear();
-			biblioteca();
-			break;
-		case '2':
-			clear();
-			queijo();
-			break;
-		case '3':
-			clear();
-			auditorios();
-			break;
-		case '4':
-			clear();
-			titanic();
-			break;
-		case '5':
-			clear();
-			grama();
-			break;
-		case '6':
-			clear();
-			cb();
-			break;
-		case '7':
-			clear();
-			elefantebranco();
-			break;
-		case '8':
-			clear();
-			ginasio();
-			break;
-		default:
-			goto invalidInputLabel;
-			break;
-	}
-}
-
-
-void biblioteca(){
-	char choice[USR_INPUT_MAX_SIZE];
-	slowPrint("Após caminhar um pouco, você chegou na entrada da Biblioteca!\n");
-	slowPrint("Agora, o que fazer?\n");
-	biblioLoop(choice);
-}	
-
-
-void queijo(){
-	char choice[USR_INPUT_MAX_SIZE];
-	slowPrint("Após caminhar um pouco, você chegou no Queijo!\n");
-	slowPrint("Agora, o que fazer?\n");
-	queijoLoop(choice);
-}
-
-void auditorios(){
-	char choice[USR_INPUT_MAX_SIZE];
-	slowPrint("Após caminhar um pouco, você chegou na entrada de acesso aos auditórios!\n");
-	slowPrint("Agora, aonde você quer ir?\n");
-	auditoriosLoop(choice);
-
-}
-
-void titanic(){
-	char choice[USR_INPUT_MAX_SIZE];
-	slowPrint("Após caminhar um pouco, você chegou no titanic!\n");
-	slowPrint("Para onde, agora?\n");
-	titanicLoop(choice);
-
-}
-
-void grama(){
-	char choice[USR_INPUT_MAX_SIZE];
-	slowPrint("Após caminhar um pouco, você chegou na grama!\n");
-	slowPrint("Agora, o que fazer?\n");
-	gramaLoop(choice);
-
-}
-
-int cb(){
-	printf("Após caminhar um pouco, você chegou no Ciclo Básico(C2)!\n");
-	printf("Para onde, agora?\n");
-	return 1;
-}
-
-int elefantebranco(){
-	printf("Após caminhar um pouco, você chegou no Elefante Branco!\n");
-	printf("Para onde, agora?\n");
-	return 1;
-}
-
-int ginasio(){
-	printf("Após caminhar um pouco, você chegou no Ginásio!\n");
-	printf("Para onde, agora?\n");
-	return 1;
-}
-
-
-void biblioLoop(char *choice){
-	printf("1)BANHEIRO DA BIBLIOTECA     2)ENTRAR NA BIBLIOTECA\n");
-	printf("3)IR PARA OUTRO LUGAR\n");
-	printf("> ");
-	
-	cleanScan(choice);
-	while(classifiesAsCommand(choice)) {
-		clear();
-		processCommand(choice);
-		// I know, I know, code repetition is an unforgivable crime or something
-		slowPrint("Mas agora, o que quer fazer na biblioteca?\n");
-			printf("1)BANHEIRO DA BIBLIOTECA     2)ENTRAR NA BIBLIOTECA\n");
-			printf("3)IR PARA OUTRO LUGAR\n");
-			printf("> ");
-		cleanScan(choice);
-	}
-	clear();
-
-	switch (toupper(*choice)){
-		case '1':
-			slowPrint("Você caminha até o banheiro da Biblioteca!\n");
-			slowPrint("Depois de fazer suas necessidades, você volta para a entrada da Biblioteca.\n");
-			slowPrint("Para onde, agora?\n");
-			biblioLoop(choice);
-			break;
-		case '2':
-			slowPrint("Você adentra na Biblioteca!\n");
-			slowPrint("Mas, percebe que ela não está funcionando agora.\n");
-			slowPrint("Para onde, então?\n");
-			biblioLoop(choice);
-			break;
-		case '3':
-			system("clear");
-			printMap();
-			showNavigationOptions();
-			break;
-		default:
-			system("clear");
-			slowPrint("Opção inválida. Escolha novamente.\n");
-			biblioLoop(choice);
-			break;			
-	}
-}
-
-void titanicLoop(char *choice){
-	printf("1)BANDECO           2)SALAS DE AULA\n");
-	printf("3)MESAS DE PAPELÃO  4)ASSENTOS DA ENTRADA\n");
-	printf("5)IR PARA OUTRO LUGAR\n");
-	printf("> ");
-	
-	cleanScan(choice);
-	while(classifiesAsCommand(choice)) {
-		clear();
-		processCommand(choice);
-		// I know, I know, code repetition is an unforgivable crime or something
-		slowPrint("E agora, para que parte do Titanic quer ir?\n");
-			printf("1)BANDECO           2)SALAS DE AULA\n");
-			printf("3)MESAS DE PAPELÃO  4)ASSENTOS DA ENTRADA\n");
-			printf("5)IR PARA OUTRO LUGAR\n");
-			printf("> ");
-		cleanScan(choice);
-	}
-	clear();
-
-	switch (toupper(*choice)){
-		case '1':
-			slowPrint("Você anda em direção ao Bandeco...\n");
-			slowPrint("Mas, ele está fechado (trancado pelo outro lado).\n");
-			system("clear");
-			slowPrint("E agora?\n");
-			titanicLoop(choice);
-			break;
-		case '2':
-			slowPrint("Você sobe as escadas e vaga pelas salas de aula...\n");
-			slowPrint("Mas, não está rolando nenhuma aula do seu curso no momento.\n");
-			system("clear");
-			slowPrint("E agora?\n");
-			titanicLoop(choice);
-			break;
-		case '3':
-			slowPrint("Você procura uma mesa de papelão e senta numa cadeira...\n");
-			slowPrint("Mas, suas costas começam a doer e você decide levantar.\n");
-			system("clear");
-			slowPrint("E agora?\n");
-			titanicLoop(choice);
-			break;
-		case '4':
-			slowPrint("Na entrada da frente do Titanic, você fica nos assentos\n");
-			slowPrint("Mas, você fica entediado e você decide levantar.\n");
-			system("clear");
-			slowPrint("E agora?\n");
-			titanicLoop(choice);
-			break;
-		case '5':
-			system("clear");
-			printMap();
-			showNavigationOptions();
-			break;
-		default:
-			system("clear");
-			slowPrint("Opção inválida. Escolha novamente.\n");
-			titanicLoop(choice);
-			break;
-	}
-}
-
-void queijoLoop(char *choice){
-	printf("1)SENTAR E REFLETIR SOBRE A VIDA    2)IR PARA OUTRO LUGAR\n");
-	printf("> ");
-	
-	cleanScan(choice);
-	while(classifiesAsCommand(choice)) {
-		clear();
-		processCommand(choice);
-		// I know, I know, code repetition is an unforgivable crime or something
-		slowPrint("A vida aqui no queijo é muito boa, o que quer fazer?\n");
-			printf("1)SENTAR E REFLETIR SOBRE A VIDA    2)IR PARA OUTRO LUGAR\n");
-			printf("> ");
-		cleanScan(choice);
-	}
-	clear();
-
-	switch(toupper(*choice)){
-		case '1':
-			slowPrint("Você senta no Queijo e começa a transcender espiritualmente...\n");
-			slowPrint("Após isso, o que você vai fazer?\n");
-			queijoLoop(choice);
-			break;
-		case '2':
-			system("clear");
-			printMap();
-			showNavigationOptions();
-			break;
-		default:
-			system("clear");
-			slowPrint("Opção inválida. Escolha novamente.\n");
-			queijoLoop(choice);
-			break;
-	}
-}
-
-void auditoriosLoop(char *choice){
-	printf("1)LANCHONETE     2)AUDITÓRIO 1\n");
-	printf("3)AUDITÓRIO 2	 4)BANHEIROS \n");
-	printf("5)IR PARA OUTRO LUGAR\n");
-	printf("> ");
-
-	cleanScan(choice);
-	while(classifiesAsCommand(choice)) {
-		clear();
-		processCommand(choice);
-		// I know, I know, code repetition is an unforgivable crime or something
-		slowPrint("Está bem vazio aqui nos auditórios, quer fazer o que?\n");
-			printf("1)LANCHONETE     2)AUDITÓRIO 1\n");
-			printf("3)AUDITÓRIO 2	 4)BANHEIROS \n");
-			printf("5)IR PARA OUTRO LUGAR\n");
-			printf("> ");
-		cleanScan(choice);
-	}
-	clear();
-
-	switch (toupper(*choice)){
-		case '1':
-			slowPrint("Você chega na lanchonete e começa a olhar os preços...\n");
-			slowPrint("Mas, percebe que os preços estão longe da realidade do brasileiro médio.\n");
-			slowPrint("E morre.\n");
-			slowPrint("Para onde, agora?\n");
-			auditoriosLoop(choice);
-			break;
-		case '2':
-			slowPrint("Você anda até o primeiro auditório...\n");
-			slowPrint("Mas não tem nada rolando lá.\n");
-			slowPrint("Então, você volta para o lado de fora.\n");
-			slowPrint("Para onde, agora?\n");
-			auditoriosLoop(choice);
-			break;
-		case '3':
-			slowPrint("Você anda até o segundo auditório...\n");
-			slowPrint("Mas não tem nada rolando lá.\n");
-			slowPrint("Então, você volta para o lado de fora.\n");
-			slowPrint("Para onde, agora?\n");
-			auditoriosLoop(choice);
-			break;
-		case '4':
-			slowPrint("Você anda até os banheiros...\n");
-			slowPrint("E tira água do joelho.\n");
-			slowPrint("Então, você volta para o lado de fora.\n");
-			slowPrint("Para onde, agora?\n");
-			auditoriosLoop(choice);
-			break;
-		case '5':
-			system("clear");
-			printMap();
-			showNavigationOptions();
-			break;
-		default:
-			system("clear");
-			slowPrint("Opção inválida. Escolha novamente.\n");
-			auditoriosLoop(choice);
-			break;
-	}
-}
-
-void gramaLoop(char *choice){
-	printf("1)PEGAR UMA CADEIRA E SENTAR   2)IR PARA OUTRO LUGAR\n");
-	printf("> ");
-	
-	cleanScan(choice);
-	while(classifiesAsCommand(choice)) {
-		clear();
-		processCommand(choice);
-		// I know, I know, code repetition is an unforgivable crime or something
-		slowPrint("você chega até a ouvir a grama chamando seu nome, o que quer fazer?\n");
-			printf("1)PEGAR UMA CADEIRA E SENTAR   2)IR PARA OUTRO LUGAR\n");
-			printf("> ");
-		cleanScan(choice);
-	}
-	clear();
-
-	switch(toupper(*choice)){
-		case '1':
-			slowPrint("Você pega uma cadeira, senta na grama e começa a relaxar...\n");
-			slowPrint("Depois de um tempo, você levanta e guarda a cadeira.\n");
-			slowPrint("O que vais fazer agora?\n");
-			gramaLoop(choice);
-			break;
-		case '2':
-			system("clear");
-			printMap();
-			showNavigationOptions();
-		default:
-			system("clear");
-			slowPrint("Opção inválida. Escolha novamente.\n");
-			gramaLoop(choice);
-			break;
-	}
-}
